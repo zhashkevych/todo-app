ad6c8eb22a1f663ad48578e6a84c6e2be5e87382
Maksim Zhashkevych <zhashkevychmaksim@gmail.com>
2020-10-24 09:43:48 +0300

    added todo lists tests & makefile test instruction

--- go.mod
+++ go.mod
@@ -9,8 +9,9 @@ require (
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/jmoiron/sqlx v1.2.0
-	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/joho/godotenv v1.3.0
 	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/lib/pq v1.0.0
 	github.com/magiconair/properties v1.8.2 // indirect
 	github.com/mitchellh/mapstructure v1.3.3 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -21,7 +22,7 @@ require (
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.7.1 // indirect
+	github.com/spf13/viper v1.7.1
 	github.com/zhashkevych/go-sqlxmock v1.5.2-0.20201023121933-f973d0041cfc
 	golang.org/x/sys v0.0.0-20200909081042-eff7692f9009 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
--- pkg/repository/todo_list_postgres_test.go
+++ pkg/repository/todo_list_postgres_test.go
@@ -1,1 +1,379 @@
 package repository
+
+import (
+	"database/sql"
+	sqlmock "github.com/zhashkevych/go-sqlxmock"
+	"github.com/zhashkevych/todo-app"
+	"reflect"
+	"testing"
+)
+
+func TestTodoListPostgres_Create(t *testing.T) {
+	db, mock, err := sqlmock.Newx()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	r := NewTodoListPostgres(db)
+
+	type args struct {
+		userId int
+		item   todo.TodoList
+	}
+	tests := []struct {
+		name    string
+		mock    func()
+		input   args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "Ok",
+			mock: func() {
+				mock.ExpectBegin()
+
+				rows := sqlmock.NewRows([]string{"id"}).AddRow(1)
+				mock.ExpectQuery("INSERT INTO todo_lists").
+					WithArgs("title", "description").WillReturnRows(rows)
+
+				mock.ExpectExec("INSERT INTO users_lists").WithArgs(1, 1).
+					WillReturnResult(sqlmock.NewResult(1, 1))
+
+				mock.ExpectCommit()
+			},
+			input: args{
+				userId: 1,
+				item: todo.TodoList{
+					Title:       "title",
+					Description: "description",
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "Empty Fields",
+			mock: func() {
+				mock.ExpectBegin()
+
+				rows := sqlmock.NewRows([]string{"id"})
+				mock.ExpectQuery("INSERT INTO todo_lists").
+					WithArgs("", "description").WillReturnRows(rows)
+
+				mock.ExpectRollback()
+			},
+			input: args{
+				userId: 1,
+				item: todo.TodoList{
+					Title:       "",
+					Description: "description",
+				},
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mock()
+
+			got, err := r.Create(tt.input.userId, tt.input.item)
+			if err != nil && !tt.wantErr {
+				t.Fatal(err)
+			}
+
+			if err == nil && got != tt.want {
+				t.Fatalf("Results mismatch; want %d, got %d", tt.want, got)
+			}
+		})
+	}
+}
+
+func TestTodoListPostgres_GetAll(t *testing.T) {
+	db, mock, err := sqlmock.Newx()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	r := NewTodoListPostgres(db)
+
+	type args struct {
+		userId int
+	}
+	tests := []struct {
+		name    string
+		mock    func()
+		input   args
+		want    []todo.TodoList
+		wantErr bool
+	}{
+		{
+			name: "Ok",
+			mock: func() {
+				rows := sqlmock.NewRows([]string{"id", "title", "description"}).
+					AddRow(1, "title1", "description1").
+					AddRow(2, "title2", "description2").
+					AddRow(3, "title3", "description3")
+
+				mock.ExpectQuery("SELECT (.+) FROM todo_lists tl INNER JOIN users_lists ul on (.+) WHERE (.+)").
+					WithArgs(1).WillReturnRows(rows)
+			},
+			input: args{
+				userId: 1,
+			},
+			want: []todo.TodoList{
+				{1, "title1", "description1"},
+				{2, "title2", "description2"},
+				{3, "title3", "description3"},
+			},
+		},
+		{
+			name: "Ok",
+			mock: func() {
+				rows := sqlmock.NewRows([]string{"id", "title", "description"}).
+					AddRow(1, "title1", "description1").
+					AddRow(2, "title2", "description2").
+					AddRow(3, "title3", "description3")
+
+				mock.ExpectQuery("SELECT (.+) FROM todo_lists tl INNER JOIN users_lists ul on (.+) WHERE (.+)").
+					WithArgs(1).WillReturnRows(rows)
+			},
+			input: args{
+				userId: 1,
+			},
+			want: []todo.TodoList{
+				{1, "title1", "description1"},
+				{2, "title2", "description2"},
+				{3, "title3", "description3"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mock()
+
+			got, err := r.GetAll(tt.input.userId)
+			if err != nil && !tt.wantErr {
+				t.Fatal(err)
+			}
+
+			if err == nil && !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("Results mismatch; want %v, got %v", tt.want, got)
+			}
+		})
+	}
+}
+
+func TestTodoListPostgres_GetById(t *testing.T) {
+	db, mock, err := sqlmock.Newx()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	r := NewTodoListPostgres(db)
+
+	type args struct {
+		listId int
+		userId int
+	}
+	tests := []struct {
+		name    string
+		mock    func()
+		input   args
+		want    todo.TodoList
+		wantErr bool
+	}{
+		{
+			name: "Ok",
+			mock: func() {
+				rows := sqlmock.NewRows([]string{"id", "title", "description"}).
+					AddRow(1, "title1", "description1")
+
+				mock.ExpectQuery("SELECT (.+) FROM todo_lists tl INNER JOIN users_lists ul on (.+) WHERE (.+)").
+					WithArgs(1, 1).WillReturnRows(rows)
+			},
+			input: args{
+				listId: 1,
+				userId: 1,
+			},
+			want: todo.TodoList{1, "title1", "description1"},
+		},
+		{
+			name: "Not Found",
+			mock: func() {
+				rows := sqlmock.NewRows([]string{"id", "title", "description"})
+
+				mock.ExpectQuery("SELECT (.+) FROM todo_lists tl INNER JOIN users_lists ul on (.+) WHERE (.+)").
+					WithArgs(1, 1).WillReturnRows(rows)
+			},
+			input: args{
+				listId: 404,
+				userId: 1,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mock()
+
+			got, err := r.GetById(tt.input.userId, tt.input.listId)
+			if err != nil && !tt.wantErr {
+				t.Fatal(err)
+			}
+
+			if err == nil && !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("Results mismatch; want %v, got %v", tt.want, got)
+			}
+		})
+	}
+}
+
+func TestTodoListPostgres_Delete(t *testing.T) {
+	db, mock, err := sqlmock.Newx()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	r := NewTodoListPostgres(db)
+
+	type args struct {
+		listId int
+		userId int
+	}
+	tests := []struct {
+		name    string
+		mock    func()
+		input   args
+		wantErr bool
+	}{
+		{
+			name: "Ok",
+			mock: func() {
+				mock.ExpectExec("DELETE FROM todo_lists tl USING users_lists ul WHERE (.+)").
+					WithArgs(1, 1).WillReturnResult(sqlmock.NewResult(0, 1))
+			},
+			input: args{
+				listId: 1,
+				userId: 1,
+			},
+		},
+		{
+			name: "Not Found",
+			mock: func() {
+				mock.ExpectExec("DELETE FROM todo_lists tl USING users_lists ul WHERE (.+)").
+					WithArgs(1, 404).WillReturnError(sql.ErrNoRows)
+			},
+			input: args{
+				listId: 404,
+				userId: 1,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mock()
+
+			err := r.Delete(tt.input.userId, tt.input.listId)
+			if err != nil && !tt.wantErr {
+				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func TestTodoListPostgres_Update(t *testing.T) {
+	db, mock, err := sqlmock.Newx()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	r := NewTodoListPostgres(db)
+
+	type args struct {
+		listId int
+		userId int
+		input  todo.UpdateListInput
+	}
+	tests := []struct {
+		name    string
+		mock    func()
+		input   args
+		wantErr bool
+	}{
+		{
+			name: "OK_AllFields",
+			mock: func() {
+				mock.ExpectExec("UPDATE todo_lists tl SET (.+) FROM users_lists ul WHERE (.+)").
+					WithArgs("new title", "new description", 1, 1).WillReturnResult(sqlmock.NewResult(0, 1))
+			},
+			input: args{
+				listId: 1,
+				userId: 1,
+				input: todo.UpdateListInput{
+					Title:       stringPointer("new title"),
+					Description: stringPointer("new description"),
+				},
+			},
+		},
+		{
+			name: "OK_WithoutDescription",
+			mock: func() {
+				mock.ExpectExec("UPDATE todo_lists tl SET (.+) FROM users_lists ul WHERE (.+)").
+					WithArgs("new title", 1, 1).WillReturnResult(sqlmock.NewResult(0, 1))
+			},
+			input: args{
+				listId: 1,
+				userId: 1,
+				input: todo.UpdateListInput{
+					Title:       stringPointer("new title"),
+				},
+			},
+		},
+		{
+			name: "OK_WithoutTitle",
+			mock: func() {
+				mock.ExpectExec("UPDATE todo_lists tl SET (.+) FROM users_lists ul WHERE (.+)").
+					WithArgs("new description", 1, 1).WillReturnResult(sqlmock.NewResult(0, 1))
+			},
+			input: args{
+				listId: 1,
+				userId: 1,
+				input: todo.UpdateListInput{
+					Description:       stringPointer("new description"),
+				},
+			},
+		},
+		{
+			name: "OK_NoInputFields",
+			mock: func() {
+				mock.ExpectExec("UPDATE todo_lists tl SET (.+) FROM users_lists ul WHERE (.+)").
+					WithArgs(1, 1).WillReturnResult(sqlmock.NewResult(0, 1))
+			},
+			input: args{
+				listId: 1,
+				userId: 1,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mock()
+
+			err := r.Update(tt.input.userId, tt.input.listId, tt.input.input)
+			if err != nil && !tt.wantErr {
+				t.Fatal(err)
+			}
+		})
+	}
+}
